0ca43f62b404841897b5d6cf031200bbc0a8e1f7
Bruno José <bru.matias100@gmail.com>
2025-11-10 14:47:09 -0300

    feat: creating text component

---
+++ src/components/text.tsx
@@ -0,0 +1,40 @@
+import { cva, type VariantProps } from 'class-variance-authority';
+import React from 'react';
+
+export const textVariants = cva('font-sans text-gray-200', {
+  variants: {
+    variant: {
+      heading: 'font-bold text-2xl',
+      subtitle: 'font-bold text-xl',
+      body: 'text-sm font-normal',
+      label: 'text-base font-normal',
+      medium: 'text-sm font-semibold',
+    },
+  },
+  defaultVariants: {
+    variant: 'body',
+  },
+});
+
+interface TextProps extends VariantProps<typeof textVariants> {
+  as?: keyof React.JSX.IntrinsicElements;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export function Text({
+  as = 'span',
+  className,
+  variant,
+  children,
+  ...props
+}: TextProps) {
+  return React.createElement(
+    as,
+    {
+      className: textVariants({ variant, className }),
+      ...props,
+    },
+    children
+  );
+}
