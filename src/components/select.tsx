5f269852e673e00c72160d5b3cb10de46f58408a
Bruno José <bru.matias100@gmail.com>
2025-11-12 17:46:08 -0300

    fix: remove twmerge from select component

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,7 +1,6 @@
 import * as SelectPrimitive from '@radix-ui/react-select';
 import { cx } from 'class-variance-authority';
 import type { ComponentProps, ReactNode } from 'react';
-import { twMerge } from 'tailwind-merge';
 
 import CaretIcon from '@/assets/icons/caret.svg?react';
 import CheckIcon from '@/assets/icons/check.svg?react';
@@ -45,10 +44,8 @@ export const SelectContent = ({
 }: SelectContentProps) => (
   <SelectPrimitive.Portal>
     <SelectPrimitive.Content
-      className={twMerge(
-        cx(
-          'data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0 data-[state=open]:zoom-in-40 data-[state=closed]:zoom-out-40 data-[state=open]:animate-in data-[state=closed]:animate-out z-50 min-w-60 rounded-lg border border-gray-300 bg-gray-500 shadow-lg'
-        ),
+      className={cx(
+        'data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0 data-[state=open]:zoom-in-40 data-[state=closed]:zoom-out-40 data-[state=open]:animate-in data-[state=closed]:animate-out z-50 min-w-60 rounded-lg border border-gray-300 bg-gray-500 shadow-lg',
         className
       )}
       position="popper"
@@ -75,8 +72,8 @@ export const SelectLabel = ({
   ...props
 }: SelectLabelProps) => (
   <SelectPrimitive.Label
-    className={twMerge(
-      cx('px-3 py-2 text-xs font-semibold text-gray-500 uppercase'),
+    className={cx(
+      'px-3 py-2 text-xs font-semibold text-gray-500 uppercase',
       className
     )}
     {...props}
@@ -95,10 +92,8 @@ export const SelectItem = ({
   ...props
 }: SelectItemProps) => (
   <SelectPrimitive.Item
-    className={twMerge(
-      cx(
-        'relative flex cursor-pointer items-center gap-2 rounded px-3 py-2 text-sm text-gray-100 hover:bg-gray-400 focus:bg-blue-50 focus:outline-none data-disabled:pointer-events-none data-disabled:opacity-50'
-      ),
+    className={cx(
+      'relative flex cursor-pointer items-center gap-2 rounded px-3 py-2 text-sm text-gray-100 hover:bg-gray-400 focus:bg-blue-50 focus:outline-none data-disabled:pointer-events-none data-disabled:opacity-50',
       className
     )}
     {...props}
@@ -115,7 +110,7 @@ export const SelectSeparator = ({
   ...props
 }: ComponentProps<typeof SelectPrimitive.Separator>) => (
   <SelectPrimitive.Separator
-    className={twMerge(cx('my-1 h-px bg-gray-200'), className)}
+    className={cx('my-1 h-px bg-gray-200', className)}
     {...props}
   />
 );
