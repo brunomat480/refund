65875304ade72c9751f8661440d3672d85a5a935
Bruno José <bru.matias100@gmail.com>
2025-11-12 20:59:47 -0300

    style: adding animete in refund request delete modal component

--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -16,7 +16,10 @@ export function DialogOverlay({
 }: ComponentProps<typeof DialogPrimitive.Overlay>) {
   return (
     <DialogPrimitive.Overlay
-      className={cx('fixed inset-0 z-50 bg-gray-100/60', className)}
+      className={cx(
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0 fixed inset-0 z-50 bg-gray-100/60',
+        className
+      )}
       {...props}
     />
   );
@@ -32,7 +35,7 @@ export function DialogContent({
       <DialogOverlay />
       <DialogPrimitive.Content
         className={cx(
-          'fixed top-1/2 left-1/2 z-50 w-full max-w-lg -translate-x-1/2 -translate-y-1/2',
+          'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-90 data-[state=open]:zoom-in-90 fixed top-1/2 left-1/2 z-50 w-full max-w-lg -translate-x-1/2 -translate-y-1/2',
           className
         )}
         {...props}
